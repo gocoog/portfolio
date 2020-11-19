a66a490a12ece4229fd49df2c78a6b5cb0484bfe
Wilmar Reyes <reyes.wilmar@gmail.com>
2020-11-19 09:58:50 -0600

    home page font changed

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -23,9 +23,12 @@ export default function Home() {
     const classes = useStyles();
     return (
         <div className="home-page">
-            <h1>Hello! I'm Wilmar Reyes</h1>
-            <h4>I'm a full stack software engineer currently based in Houston, TX</h4>
-            <h4>Feel free to look around my site and let's connect!</h4>
+          <div className="typewriter">
+          <div className="typewriter-text">Hello! I'm Wilmar Reyes</div>
+          </div>
+            
+            <h4 className="home-desc">I'm a full stack software engineer currently based in Houston, TX</h4>
+            <h4 className="home-desc">Feel free to look around my site and let's connect!</h4>
             <Link to="/projects" className={classes.textlink}>
             <Button className={classes.button} variant="outlined">View Projects</Button>
             </Link>
